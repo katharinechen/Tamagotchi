632807aa889a72585499d33e0c6fe4022e08f069
Katharine Chen & Austin Steward <student@epicodus.com>
2014-07-17 16:12:47 -0700

    A working, breathing, amazingly awesome Tamagotchi game that everyone in their right mind should play daily

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,76 @@ var Tamagotchi = {
 
   timePasses: function() {
     this.foodLevel -= 1;
+    this.sleepLevel -= 1;
+    this.activityLevel -= 1;
+    $("#feed").text(this.foodLevel);
+    $("#purple").text(this.activityLevel);
+    $("#rest").text(this.sleepLevel);
   },
 
   isAlive: function() {
 
-    if (this.foodLevel > 0) {
+    if ((this.foodLevel > 0) && (this.sleepLevel > 0) && (this.activityLevel > 0)) {
       return true;
     } else {
-      return  false;
+      $("#fadeout").fadeOut();
+      $("#you-died").fadeIn();
+
     }
   }
 }
 
 
+$(document).ready(function() {
+  $("#feed").text(10);
+  $("#purple").text(10);
+  $("#rest").text(10);
+  $("#nameSpace").show();
+
+  $("#nameSpace").submit(function(event) {
+    var cutePet = Object.create(Tamagotchi);
+    cutePet.initialize($("#name").val());
+    if((cutePet.name === "hammer") || (cutePet.name === "Hammer")) {
+      $("img").show();
+    }
+
+    $("#nameSpace").hide();
+    $("#amazing-name").text(" " + cutePet.name);
+
+    setInterval(function(){ cutePet.timePasses() },500);
+    setInterval(function(){ cutePet.isAlive() },500);
+
+    event.preventDefault();
+
+    $("#food").click(function(){
+      cutePet.foodLevel += 1;
+      if(cutePet.foodLevel > 10) {
+        cutePet.foodLevel = 10;
+      }
+      $("#feed").text(cutePet.foodLevel);
+    });
+
+    $("#play").click(function(){
+      cutePet.activityLevel += 1;
+      if(cutePet.activityLevel > 10) {
+        cutePet.activityLevel = 10;
+      }
+      $("#purple").text(cutePet.activityLevel);
+    });
+
+    $("#sleep").click(function(){
+      cutePet.sleepLevel += 1;
+      if(cutePet.sleepLevel > 10) {
+        cutePet.sleepLevel = 10;
+      }
+      $("#rest").text(cutePet.sleepLevel);
+    });
+
+
+
+  });
+
+
+
+
+});
